ad81453e2a9130584c6b2dd67fab7d12f60157ee
wanony <whereweat2018@gmail.com>
2021-07-04 21:29:19 +0100

    Changed case sensitivity

--- jsons_to_database.py
+++ jsons_to_database.py
@@ -37,39 +37,39 @@ for user in users:
         usr_cont = contri_dict[str(user)]['cont']
     else:
         usr_cont = 0
-    sql = "INSERT INTO Users(UserId, Xp, Cont) VALUES (%s, %s, %s);"
+    sql = "INSERT INTO users(UserId, Xp, Cont) VALUES (%s, %s, %s);"
     vals = (user, users[user]['xp'], usr_cont)
     cursor.execute(sql, vals)
 
 for group in gfys_dict["groups"]:
     # add group to groupz table
-    sql = "INSERT INTO Groupz(RomanName, AddedBy) VALUES (%s, %s);"
+    sql = "INSERT INTO groupz(RomanName, AddedBy) VALUES (%s, %s);"
     vals = (group, my_id)
     cursor.execute(sql, vals)
     # format group into string for query
-    sel_sql = "SELECT GroupId FROM Groupz WHERE RomanName = (%s);"
+    sel_sql = "SELECT GroupId FROM groupz WHERE RomanName = (%s);"
     cursor.execute(sel_sql, (group,))
 
     group_id = cursor.fetchone()[0]
-    alias_sql = "INSERT INTO Groupz_Aliases(GroupId, Alias, AddedBy) VALUES (%s, %s, %s)"
+    alias_sql = "INSERT INTO groupz_aliases(GroupId, Alias, AddedBy) VALUES (%s, %s, %s)"
     vals = (group_id, group, my_id)
     cursor.execute(alias_sql, vals)
     # iter to add members to members table
     for member in gfys_dict["groups"][group]:
         msql = """
-             INSERT INTO Members(GroupId, RomanName, AddedBy) VALUES (%s, %s, %s);
+             INSERT INTO members(GroupId, RomanName, AddedBy) VALUES (%s, %s, %s);
              """
         val = (group_id, member, my_id)
         cursor.execute(msql, val)
         # string for member name
-        sel_sql = """SELECT MemberId FROM Members
-                        left JOIN Groupz ON Groupz.GroupId = Members.GroupId
-                        WHERE Members.RomanName = %s
-                        AND Groupz.GroupId = %s"""
+        sel_sql = """SELECT MemberId FROM members
+                        left JOIN groupz ON groupz.GroupId = members.GroupId
+                        WHERE members.RomanName = %s
+                        AND groupz.GroupId = %s"""
         cursor.execute(sel_sql, (member, group_id))
 
         memberid = cursor.fetchone()[0]
-        alias_sql = "INSERT INTO Member_Aliases(MemberId, Alias, AddedBy) VALUES (%s, %s, %s)"
+        alias_sql = "INSERT INTO member_aliases(MemberId, Alias, AddedBy) VALUES (%s, %s, %s)"
         vals = (memberid, member, my_id)
         try:
             cursor.execute(alias_sql, vals)
@@ -85,15 +85,15 @@ for group in gfys_dict["groups"]:
                     split[-1] = split[-1].split("-")
                     split[-1] = split[-1][0]
                 link = "https://gfycat.com/" + split[-1]
-            lsql = "INSERT INTO Links(Link, AddedBy) VALUES (%s, %s);"
+            lsql = "INSERT INTO links(Link, AddedBy) VALUES (%s, %s);"
             valz = (link, my_id)
             cursor.execute(lsql, valz)
             # MIGHT BE SOME ERROR HERE #############################################################
-            lidsql = "SELECT * FROM Links WHERE Link = (%s);"
+            lidsql = "SELECT * FROM links WHERE Link = (%s);"
             cursor.execute(lidsql, (link,))
             lid = cursor.fetchone()[0]
             # add to link_members table
-            lmsql = "INSERT INTO Link_Members(LinkId, MemberId) VALUES (%s, %s);"
+            lmsql = "INSERT INTO link_members(LinkId, MemberId) VALUES (%s, %s);"
             try:
                 cursor.execute(lmsql, (lid, memberid))
             except errors.IntegrityError as e:
@@ -103,9 +103,9 @@ for group in gfys_dict["groups"]:
 
 
 for tag in gfys_dict["tags"]:
-    sql = "INSERT INTO Tags(TagName, Addedby) VALUES (%s, %s);"
-    get_tag_id = "SELECT TagId FROM Tags WHERE TagName = (%s)"
-    alias_sql = "INSERT INTO Tag_Aliases(TagId, Alias, AddedBy) VALUES (%s, %s, %s)"
+    sql = "INSERT INTO tags(TagName, Addedby) VALUES (%s, %s);"
+    get_tag_id = "SELECT TagId FROM tags WHERE TagName = (%s)"
+    alias_sql = "INSERT INTO tag_aliases(TagId, Alias, AddedBy) VALUES (%s, %s, %s)"
     cursor.execute(sql, (tag, my_id))
     cursor.execute(get_tag_id, (tag,))
     t_id = cursor.fetchone()[0]
@@ -121,7 +121,7 @@ for tag in gfys_dict["tags"]:
                 split[-1] = split[-1].split("-")
                 split[-1] = split[-1][0]
             link = "https://gfycat.com/" + split[-1]
-        jkl = "SELECT * FROM Links WHERE Link = (%s);"
+        jkl = "SELECT * FROM links WHERE Link = (%s);"
         cursor.execute(jkl, (link,))
         linkrow = cursor.fetchone()
         if linkrow is not None:
@@ -129,11 +129,11 @@ for tag in gfys_dict["tags"]:
         else:
             continue
 
-        tkl = """SELECT * FROM Tags
+        tkl = """SELECT * FROM tags
             WHERE TagName = (%s);"""
         cursor.execute(tkl, (tag,))
         t_id = cursor.fetchone()[0]
-        ejif = "INSERT INTO Link_Tags(LinkId, TagId) VALUES (%s, %s);"
+        ejif = "INSERT INTO link_tags(LinkId, TagId) VALUES (%s, %s);"
         values = (l_id, t_id)
         try:
             cursor.execute(ejif, values)
@@ -141,18 +141,18 @@ for tag in gfys_dict["tags"]:
             print(e)
 
 
-channel_sql = "INSERT INTO Channels(Channel) VALUES (%s);"
+channel_sql = "INSERT INTO channels(Channel) VALUES (%s);"
 for reddit in reddit_dict:
-    reddit_sql = "INSERT INTO Reddit (RedditName) VALUES (%s);"
+    reddit_sql = "INSERT INTO reddit (RedditName) VALUES (%s);"
     cursor.execute(reddit_sql, (reddit,))
     channels = reddit_dict[reddit]["channels"]
-    rsql = "INSERT INTO Reddit_Channels(ChannelId, RedditId) VALUES (%s, %s);"
+    rsql = "INSERT INTO reddit_channels(ChannelId, RedditId) VALUES (%s, %s);"
     for channel in channels:
         cursor.execute(channel_sql, (channel,))
-        select_sql = "SELECT * FROM Channels WHERE Channel = (%s);"
+        select_sql = "SELECT * FROM channels WHERE Channel = (%s);"
         cursor.execute(select_sql, (channel,))
         channel_id = cursor.fetchone()[0]
-        select_red = "SELECT * FROM Reddit WHERE RedditName = (%s);"
+        select_red = "SELECT * FROM reddit WHERE RedditName = (%s);"
         cursor.execute(select_red, (reddit,))
         reddit_id = cursor.fetchone()[0]
         cursor.execute(rsql, (channel_id, reddit_id))
@@ -160,23 +160,23 @@ for reddit in reddit_dict:
 
 for channel in auditing_dict["auditing_channels"]:
     # check if channel exists
-    isitthere = "SELECT * FROM Channels WHERE Channel = (%s);"
+    isitthere = "SELECT * FROM channels WHERE Channel = (%s);"
     cursor.execute(isitthere, (channel,))
     if cursor.fetchone() is None:
         cursor.execute(channel_sql, (channel,))
         cursor.execute(isitthere, (channel,))
         c_id = cursor.fetchone()[0]
-        s = "INSERT INTO Auditing_Channels(ChannelId) VALUES (%s);"
+        s = "INSERT INTO auditing_channels(ChannelId) VALUES (%s);"
         cursor.execute(s, (c_id,))
     else:
         c_id = cursor.fetchone()[0]
-        s = "INSERT INTO Auditing_Channels(ChannelId) VALUES (%s);"
+        s = "INSERT INTO auditing_channels(ChannelId) VALUES (%s);"
         cursor.execute(s, (c_id,))
 
 
 for command in custom_dict["commands"]:
     command_link = custom_dict["commands"][command]
-    sql = "INSERT INTO Custom_Commands(CommandName, Command, AddedBy) VALUES (%s, %s, %s);"
+    sql = "INSERT INTO custom_commands(CommandName, Command, AddedBy) VALUES (%s, %s, %s);"
     vals = (command, command_link, my_id)
     cursor.execute(sql, vals)
 
