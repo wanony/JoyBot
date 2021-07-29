595506d5942d2c27896b3bc1f4c6e6ac896aa7cb
wanony <whereweat2018@gmail.com>
2021-07-30 00:45:00 +0100

    Attempt to dodge rate limit with praw

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -8,6 +8,14 @@ from data import apis_dict
 from embeds import success_embed, error_embed
 
 
+def get_and_format_subs_list():
+    return [x[0] for x in get_all_subreddits()]
+
+
+def get_and_format_channels_with_sub(sub_name):
+    return [x[0] for x in get_channels_with_sub(sub_name)]
+
+
 class Reddits(commands.Cog):
     """Get new posts from your favourite Subreddits
     """
@@ -21,88 +29,54 @@ class Reddits(commands.Cog):
                                        client_secret=apis_dict["reddit_secret"],
                                        user_agent="idk what this is")
 
-    @commands.Cog.listener()
     async def post_new(self):
         await self.disclient.wait_until_ready()
         while not self.disclient.is_closed():
-            subs_list = [x[0] for x in get_all_subreddits()]
-            if not subs_list:
-                continue
-            try:
-                for subs in subs_list:
-                    sub = await self.reddit.subreddit(subs)
-                    async for subm in sub.new(limit=5):
-                        titl = subm.title
-                        if "/r/" in subm.url:
-                            url = ""
-                        else:
-                            url = subm.url
-                        auth = subm.author
-                        perm = subm.permalink
-                        fts = (".JPG", ".jpg", ".JPEG",
-                               ".jpeg", ".PNG", ".png")
-                        gifs = (
-                            "https://gfycat.com/",
-                            "https://www.redgifs.com/",
-                            "https://www.gifdeliverynetwork.com/"
-                        )
-                        channels_with_reddit = [x[0] for x in get_channels_with_sub(subs)]
-                        if not channels_with_reddit:
+            subs_list = get_and_format_subs_list()
+            for subs in subs_list:
+                channels_with_reddit = get_and_format_channels_with_sub(subs)
+                if not channels_with_reddit:
+                    continue
+                # do as much processing before reddit call
+                sub = await self.reddit.subreddit(subs)
+                async for subm in sub.new(limit=5):
+                    titl = subm.title
+                    if "/r/" in subm.url:
+                        url = ""
+                    else:
+                        url = subm.url
+                    auth = subm.author
+                    perm = subm.permalink
+                    fts = (".JPG", ".jpg", ".JPEG",
+                           ".jpeg", ".PNG", ".png")
+                    gifs = (
+                        "https://gfycat.com/",
+                        "https://www.redgifs.com/",
+                        "https://www.gifdeliverynetwork.com/"
+                    )
+                    for channels in channels_with_reddit:
+                        if subs not in self.recent_posts:
+                            self.recent_posts.update({subs: {}})
+                        if channels not in self.recent_posts[subs]:
+                            self.recent_posts[subs].update({channels: []})
+                        lp = self.recent_posts[subs][channels]
+                        channel = self.disclient.get_channel(int(channels))
+                        if perm in lp:
                             continue
-                        for channels in channels_with_reddit:
-                            if subs not in self.recent_posts:
-                                dic = {subs: {}}
-                                self.recent_posts.update(dic)
-                            if channels not in self.recent_posts[subs]:
-                                dic = {channels: []}
-                                self.recent_posts[subs].update(dic)
-                            lp = self.recent_posts[subs][channels]
-                            channel = self.disclient.get_channel(int(channels))
-                            if perm in lp:
-                                continue
-                            else:
-                                soy = "https://reddit.com"
-                                if len(lp) > 10:
-                                    lp.pop(0)
-                                #  Embeds from this point
-                                desc = f"Posted by {auth} in **/r/{subs}**"
-                                clr = discord.Color.blurple()
-                                embed = discord.Embed(title=titl,
-                                                      description=desc,
-                                                      color=clr)
-                                if url:
-                                    val = f"{soy}{perm} \n**{url}**"
-                                    if url.endswith(fts) or "gallery" in url:
-                                        embed.set_image(url=url)
-                                        embed.add_field(name="Post Permalink",
-                                                        value=val)
-                                        try:
-                                            await channel.send(embed=embed)
-                                        except AttributeError:
-                                            self.recent_posts[subs].pop(channels)
-                                            print("Channel deleted")
-
-                                    elif url.startswith(gifs):
-                                        embed.add_field(name="Post Permalink",
-                                                        value=val)
-                                        try:
-                                            await channel.send(embed=embed)
-                                            await channel.send(url)
-                                        except AttributeError:
-                                            self.recent_posts[subs].pop(channels)
-                                            print("Channel deleted")
-                                    else:
-                                        val = f"{soy}{perm}"
-                                        embed.add_field(name="Post Permalink",
-                                                        value=val)
-                                        try:
-                                            await channel.send(embed=embed)
-                                            await channel.send(url)
-                                        except AttributeError:
-                                            self.recent_posts[subs].pop(channels)
-                                            print("Channel deleted")
-                                else:
-                                    val = f"{soy}{perm}"
+                        else:
+                            soy = "https://reddit.com"
+                            if len(lp) > 10:
+                                lp.pop(0)
+                            #  Embeds from this point
+                            desc = f"Posted by {auth} in **/r/{subs}**"
+                            clr = discord.Color.blurple()
+                            embed = discord.Embed(title=titl,
+                                                  description=desc,
+                                                  color=clr)
+                            if url:
+                                val = f"{soy}{perm} \n**{url}**"
+                                if url.endswith(fts) or "gallery" in url:
+                                    embed.set_image(url=url)
                                     embed.add_field(name="Post Permalink",
                                                     value=val)
                                     try:
@@ -110,12 +84,36 @@ class Reddits(commands.Cog):
                                     except AttributeError:
                                         self.recent_posts[subs].pop(channels)
                                         print("Channel deleted")
-                                lp.append(perm)
-                    await asyncio.sleep(600)
-            except Exception as e:
-                print(f'Reddit Exception: {e}')
-            await asyncio.sleep(100)
-        # print("calling reddit")
+                                elif url.startswith(gifs):
+                                    embed.add_field(name="Post Permalink",
+                                                    value=val)
+                                    try:
+                                        await channel.send(embed=embed)
+                                        await channel.send(url)
+                                    except AttributeError:
+                                        self.recent_posts[subs].pop(channels)
+                                        print("Channel deleted")
+                                else:
+                                    val = f"{soy}{perm}"
+                                    embed.add_field(name="Post Permalink",
+                                                    value=val)
+                                    try:
+                                        await channel.send(embed=embed)
+                                        await channel.send(url)
+                                    except AttributeError:
+                                        self.recent_posts[subs].pop(channels)
+                                        print("Channel deleted")
+                            else:
+                                val = f"{soy}{perm}"
+                                embed.add_field(name="Post Permalink",
+                                                value=val)
+                                try:
+                                    await channel.send(embed=embed)
+                                except AttributeError:
+                                    self.recent_posts[subs].pop(channels)
+                                    print(f"Channel deleted")
+                            lp.append(perm)
+            await asyncio.sleep(600)
 
     @commands.command()
     async def unfollow_subreddit(self, ctx, subreddit):
